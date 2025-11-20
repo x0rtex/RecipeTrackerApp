4788bdfbd65bada5eb7308bf544d16b2fb51eafd
x0rtex <s00253368@atu.ie>
2025-11-20 22:35:54 +0000

    Added recipe list to view model

--- app/src/main/java/com/x0rtex/recipetrackerapp/viewmodel/RecipeViewModel.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/viewmodel/RecipeViewModel.kt
@@ -1,7 +1,12 @@
 package com.x0rtex.recipetrackerapp.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.x0rtex.recipetrackerapp.data.models.RecipeEntity
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class RecipeViewModel() : ViewModel() {
-
+    private val _recipes = MutableStateFlow<List<RecipeEntity>>(emptyList())
+    val recipes: StateFlow<List<RecipeEntity>> = _recipes.asStateFlow()
 }
