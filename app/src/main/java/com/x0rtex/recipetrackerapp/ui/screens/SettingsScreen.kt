839187619b038626712c167316e5c045a2d23cc3
x0rtex <s00253368@atu.ie>
2025-11-20 21:14:44 +0000

    Added composables and previews to screens with text

--- app/src/main/java/com/x0rtex/recipetrackerapp/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/ui/screens/SettingsScreen.kt
@@ -1,2 +1,20 @@
 package com.x0rtex.recipetrackerapp.ui.screens
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.x0rtex.recipetrackerapp.R
+import com.x0rtex.recipetrackerapp.viewmodel.RecipeViewModel
+
+@Composable
+fun SettingsScreen(viewModel: RecipeViewModel) {
+    Text(stringResource(R.string.settings))
+}
+
+@Preview
+@Composable
+fun SettingsScreenPreview() {
+    val viewModel = RecipeViewModel()
+    SettingsScreen(viewModel)
+}
