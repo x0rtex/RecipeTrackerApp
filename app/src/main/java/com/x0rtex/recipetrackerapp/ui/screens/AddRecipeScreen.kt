42ff460c13f8971c3068c257be3c3b1ad582d1b3
x0rtex <s00253368@atu.ie>
2025-11-20 22:37:15 +0000

    Adjusted empty screens to have modifier, navController, and viewModel as parameters

--- app/src/main/java/com/x0rtex/recipetrackerapp/ui/screens/AddRecipeScreen.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/ui/screens/AddRecipeScreen.kt
@@ -2,19 +2,18 @@ package com.x0rtex.recipetrackerapp.ui.screens
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import com.x0rtex.recipetrackerapp.R
 import com.x0rtex.recipetrackerapp.viewmodel.RecipeViewModel
 
 @Composable
-fun AddRecipeScreen(viewModel: RecipeViewModel) {
+fun AddRecipeScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    viewModel: RecipeViewModel
+) {
     Text(stringResource(R.string.recipe_add))
-}
-
-@Preview
-@Composable
-fun AddRecipeScreenPreview() {
-    val viewModel = RecipeViewModel()
-    AddRecipeScreen(viewModel)
 }
--- app/src/main/java/com/x0rtex/recipetrackerapp/ui/screens/ViewRecipeScreen.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/ui/screens/ViewRecipeScreen.kt
@@ -2,19 +2,18 @@ package com.x0rtex.recipetrackerapp.ui.screens
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import com.x0rtex.recipetrackerapp.R
 import com.x0rtex.recipetrackerapp.viewmodel.RecipeViewModel
 
 @Composable
-fun ViewRecipeScreen(viewModel: RecipeViewModel) {
+fun ViewRecipeScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    viewModel: RecipeViewModel
+) {
     Text(stringResource(R.string.recipe_view))
-}
-
-@Preview
-@Composable
-fun ViewRecipeScreenPreview() {
-    val viewModel = RecipeViewModel()
-    ViewRecipeScreen(viewModel)
 }
