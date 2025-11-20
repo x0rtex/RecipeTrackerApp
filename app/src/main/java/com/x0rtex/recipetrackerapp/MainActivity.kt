78e5a21393eff06708cb98af4f11e1b36a1041ca
x0rtex <s00253368@atu.ie>
2025-11-20 20:38:07 +0000

    Removed template and added viewmodel to MainActivity

--- app/src/main/java/com/x0rtex/recipetrackerapp/MainActivity.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/MainActivity.kt
@@ -5,13 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.x0rtex.recipetrackerapp.ui.theme.RecipeTrackerAppTheme
+import com.x0rtex.recipetrackerapp.viewmodel.RecipeViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +16,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipeTrackerAppTheme {
+                val recipeViewModel: RecipeViewModel = viewModel()
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecipeTrackerAppTheme {
-        Greeting("Android")
     }
 }
