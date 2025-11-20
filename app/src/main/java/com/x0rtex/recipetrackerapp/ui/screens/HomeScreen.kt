e6511ccad18df97ed13d4cccec27776c88bab280
x0rtex <s00253368@atu.ie>
2025-11-20 22:37:37 +0000

    Added lazy column of recipes to home screen from view model state

--- app/src/main/java/com/x0rtex/recipetrackerapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/ui/screens/HomeScreen.kt
@@ -1,20 +1,32 @@
 package com.x0rtex.recipetrackerapp.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.x0rtex.recipetrackerapp.R
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import com.x0rtex.recipetrackerapp.viewmodel.RecipeViewModel
 
 @Composable
-fun HomeScreen(viewModel: RecipeViewModel) {
-    Text(stringResource(R.string.app_name))
-}
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    viewModel: RecipeViewModel
+) {
+    Column(
+        modifier = modifier
+    ) {
+        val recipes by viewModel.recipes.collectAsState()
 
-@Preview
-@Composable
-fun HomeScreenPreview() {
-    val viewModel = RecipeViewModel()
-    HomeScreen(viewModel)
+        LazyColumn {
+            items(recipes) { recipe ->
+                Text(recipe.title)
+            }
+        }
+    }
 }
