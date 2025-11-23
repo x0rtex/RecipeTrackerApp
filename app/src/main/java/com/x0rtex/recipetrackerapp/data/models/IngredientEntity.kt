cdec1ad5f773f25e7bb6c7b1cc1adba5dc573fb5
x0rtex <s00253368@atu.ie>
2025-11-23 19:03:48 +0000

    Added amount and calories properties to ingredient entity

--- app/src/main/java/com/x0rtex/recipetrackerapp/data/models/IngredientEntity.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/data/models/IngredientEntity.kt
@@ -1,6 +1,11 @@
 package com.x0rtex.recipetrackerapp.data.models
 
+import androidx.room.Entity
+
+@Entity(tableName = "ingredients")
 data class IngredientEntity(
     val id: Int = 0,
     val title: String,
+    val amount: Int?,
+    val calories: Int?
 )
