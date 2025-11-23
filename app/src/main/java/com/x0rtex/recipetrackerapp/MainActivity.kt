d0eea2da6720bafe3baa228c9801f0ae13d5ca7b
x0rtex <s00253368@atu.ie>
2025-11-23 19:05:11 +0000

    Removed redundant code from MainActivity

--- app/src/main/java/com/x0rtex/recipetrackerapp/MainActivity.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.x0rtex.recipetrackerapp.ui.theme.RecipeTrackerAppTheme
-import com.x0rtex.recipetrackerapp.viewmodel.RecipeViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +15,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipeTrackerAppTheme {
-                val recipeViewModel: RecipeViewModel = viewModel()
-
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     RecipeApp()
                 }
