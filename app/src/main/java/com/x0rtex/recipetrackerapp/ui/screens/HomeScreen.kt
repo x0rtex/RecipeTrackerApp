829e1f40be68a0dbf7182baed6dc140ba70fcca5
x0rtex <s00253368@atu.ie>
2025-11-23 19:18:20 +0000

    Removed redundant import

--- app/src/main/java/com/x0rtex/recipetrackerapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/ui/screens/HomeScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.x0rtex.recipetrackerapp.viewmodel.RecipeViewModel
 
