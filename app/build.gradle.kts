c7683ebce6a84c63430f7aa8d353c42a4f01d307
x0rtex <s00253368@atu.ie>
2025-11-23 19:03:10 +0000

    Added room dependency for database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.compose.material.icons.core)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.room.common.jvm)
 
     testImplementation(libs.junit)
     
