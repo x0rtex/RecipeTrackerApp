abd1ff452eaa245db41c7de04a03a426d6258e70
x0rtex <s00253368@atu.ie>
2025-11-26 21:54:05 +0000

    Added CRUD and selected recipe state to recipe viewmodel

--- app/src/main/java/com/x0rtex/recipetrackerapp/viewmodel/RecipeViewModel.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/viewmodel/RecipeViewModel.kt
@@ -1,12 +1,59 @@
 package com.x0rtex.recipetrackerapp.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.x0rtex.recipetrackerapp.data.models.RecipeEntity
+import com.x0rtex.recipetrackerapp.data.repository.RecipeRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class RecipeViewModel() : ViewModel() {
+class RecipeViewModel(
+    application: Application
+) : AndroidViewModel(application) {
+    private val repo = RecipeRepository(application.applicationContext)
+
     private val _recipes = MutableStateFlow<List<RecipeEntity>>(emptyList())
     val recipes: StateFlow<List<RecipeEntity>> = _recipes.asStateFlow()
+
+    private val _selectedRecipe = MutableStateFlow<RecipeEntity?>(null)
+    val selectedRecipe: StateFlow<RecipeEntity?> = _selectedRecipe.asStateFlow()
+
+    init {
+        loadAllRecipes()
+    }
+
+    fun loadAllRecipes() {
+        viewModelScope.launch {
+            repo.getAllRecipes().collect {
+                _recipes.value = it
+            }
+        }
+    }
+
+    fun loadRecipe(id: Int) {
+        viewModelScope.launch {
+            _selectedRecipe.value = repo.getRecipe(id)
+        }
+    }
+
+    fun addRecipe(recipe: RecipeEntity) {
+        viewModelScope.launch {
+            repo.insertRecipe(recipe)
+        }
+    }
+
+    fun updateRecipe(recipe: RecipeEntity) {
+        viewModelScope.launch {
+            repo.updateRecipe(recipe)
+        }
+    }
+
+    fun deleteRecipe(recipe: RecipeEntity) {
+        viewModelScope.launch {
+            repo.deleteRecipe(recipe)
+        }
+    }
 }
