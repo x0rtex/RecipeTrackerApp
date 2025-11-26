b66d177821edd023ab5599bc607ba0c83a8f806a
x0rtex <s00253368@atu.ie>
2025-11-26 21:54:22 +0000

    Added database annotations to recipe entity and ingredient entity

--- app/src/main/java/com/x0rtex/recipetrackerapp/data/models/IngredientEntity.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/data/models/IngredientEntity.kt
@@ -1,10 +1,13 @@
 package com.x0rtex.recipetrackerapp.data.models
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "ingredients")
 data class IngredientEntity(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
+    val recipeId: Int,
     val title: String,
     val amount: Int?,
     val calories: Int?
--- app/src/main/java/com/x0rtex/recipetrackerapp/data/models/RecipeEntity.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/data/models/RecipeEntity.kt
@@ -1,14 +1,16 @@
 package com.x0rtex.recipetrackerapp.data.models
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "recipes")
 data class RecipeEntity(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val title: String,
     val description: String,
-    val ingredients: MutableList<IngredientEntity>,
-    val instructions: MutableList<String>,
-    val image: Int,
+    val ingredients: List<IngredientEntity>,
+    val instructions: List<String>,
+    val image: String? = null,
     val servings: Int,
 )
