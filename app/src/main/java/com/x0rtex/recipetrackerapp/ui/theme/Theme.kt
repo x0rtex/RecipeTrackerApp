81fa621a45b4e1d655555d8e837d2e217bc03f21
x0rtex <s00253368@atu.ie>
2025-11-26 21:53:44 +0000

    Unused import

--- app/src/main/java/com/x0rtex/recipetrackerapp/ui/theme/Theme.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.x0rtex.recipetrackerapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
