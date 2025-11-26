550e98fa5eba3b73d090cd79b70df134e20c4b57
x0rtex <s00253368@atu.ie>
2025-11-26 21:52:09 +0000

    Added recipe repository and empty recipe search bar

---
+++ app/src/main/java/com/x0rtex/recipetrackerapp/data/repository/RecipeRepository.kt
@@ -0,0 +1,18 @@
+package com.x0rtex.recipetrackerapp.data.repository
+
+import android.content.Context
+import com.x0rtex.recipetrackerapp.data.database.RecipeDatabase
+import com.x0rtex.recipetrackerapp.data.models.RecipeEntity
+import kotlinx.coroutines.flow.Flow
+
+class RecipeRepository(
+    context: Context
+) {
+    private val recipeDao = RecipeDatabase.get(context).recipeDao()
+
+    fun getAllRecipes(): Flow<List<RecipeEntity>> = recipeDao.getAll()
+    suspend fun getRecipe(id: Int) = recipeDao.getRecipe(id)
+    suspend fun insertRecipe(recipe: RecipeEntity) = recipeDao.insert(recipe)
+    suspend fun updateRecipe(recipe: RecipeEntity) = recipeDao.update(recipe)
+    suspend fun deleteRecipe(recipe: RecipeEntity) = recipeDao.delete(recipe)
+}
---
+++ app/src/main/java/com/x0rtex/recipetrackerapp/ui/components/RecipeSearchBar.kt
@@ -0,0 +1,16 @@
+package com.x0rtex.recipetrackerapp.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RecipeSearchBar(
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+    ) {
+
+    }
+}
