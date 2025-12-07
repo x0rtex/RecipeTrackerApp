aa356b3fe7966d5d82925b555c0108424f284b59
x0rtex <s00253368@atu.ie>
2025-12-07 14:36:41 +0000

    Allow destructive migration in database

--- app/src/main/java/com/x0rtex/recipetrackerapp/data/database/RecipeDatabase.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/data/database/RecipeDatabase.kt
@@ -5,14 +5,14 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.x0rtex.recipetrackerapp.data.Converters
 import com.x0rtex.recipetrackerapp.data.dao.RecipeDao
 import com.x0rtex.recipetrackerapp.data.models.IngredientEntity
 import com.x0rtex.recipetrackerapp.data.models.RecipeEntity
 
 @Database(
     entities = [RecipeEntity::class, IngredientEntity::class],
-    version = 1,
+    version = 2,
+    exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class RecipeDatabase : RoomDatabase() {
@@ -28,7 +28,8 @@ abstract class RecipeDatabase : RoomDatabase() {
                     context.applicationContext,
                     RecipeDatabase::class.java,
                     "recipes.db"
-                ).build().also { INSTANCE = it }
+                ).fallbackToDestructiveMigration(true)
+                .build().also { INSTANCE = it }
             }
         }
     }
