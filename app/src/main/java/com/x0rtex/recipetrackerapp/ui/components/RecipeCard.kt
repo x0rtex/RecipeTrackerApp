b5f8eb88af2ae4cf161c2e9dc82d1374c3437eef
x0rtex <s00253368@atu.ie>
2025-12-07 14:38:57 +0000

    Recipe card now shows image

--- app/src/main/java/com/x0rtex/recipetrackerapp/ui/components/RecipeCard.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/ui/components/RecipeCard.kt
@@ -1,20 +1,30 @@
 package com.x0rtex.recipetrackerapp.ui.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
+import coil.compose.AsyncImage
 import com.x0rtex.recipetrackerapp.data.models.RecipeEntity
 
 @Composable
@@ -27,18 +37,24 @@ fun RecipeCard(
         modifier = modifier
             .fillMaxWidth()
             .clickable(onClick = onClick),
-        shape = RoundedCornerShape(size = 12.dp)
+        shape = RoundedCornerShape(size = 12.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ) {
         Row(
-            modifier = modifier,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(space = 12.dp),
         ) {
-            Image(
-                modifier = Modifier
-                    .size(size = 72.dp)
-                    .clip(shape = RoundedCornerShape(size = 10.dp)),
-                painter = painterResource(id = android.R.drawable.ic_menu_gallery),  // TODO: Change to image url
-                contentDescription = null,
+            // Recipe Card Image
+            RecipeCardImage(
+                imageUri = recipe.image,
+                contentDescription = recipe.title,
+                modifier = Modifier.size(size = 96.dp)
             )
+
+            // Recipe Card Info
             Column(
                 modifier = modifier,
             ) {
@@ -49,6 +65,44 @@ fun RecipeCard(
                 Text(
                     modifier = modifier,
                     text = recipe.description,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun RecipeCardImage(
+    imageUri: String?,
+    contentDescription: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(size = 8.dp),
+        colors = CardDefaults.cardColors(
+        containerColor = MaterialTheme.colorScheme.surfaceVariant
+    )
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            if (imageUri != null) {
+                // Load image
+                AsyncImage(
+                    model = imageUri.toUri(),
+                    contentDescription = contentDescription,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            } else {
+                // Show placeholder
+                Icon(
+                    imageVector = Icons.Default.Info,
+                    contentDescription = null,
+                    modifier = Modifier.size(size = 36.dp),
+                    tint = MaterialTheme.colorScheme.primary
                 )
             }
         }
