282616a59313d7d378470bc19a486f52321c44c9
x0rtex <s00253368@atu.ie>
2025-12-07 14:37:10 +0000

    Added functions to get recipe count, recipes without images and recipe search in Dao

--- app/src/main/java/com/x0rtex/recipetrackerapp/data/dao/RecipeDao.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/data/dao/RecipeDao.kt
@@ -11,18 +11,35 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RecipeDao {
+    // Get All Recipes
     @Query("SELECT * FROM recipes ORDER BY id DESC")
     fun getAll(): Flow<List<RecipeEntity>>
 
+    // Get Recipe By Id
     @Query("SELECT * FROM recipes WHERE id = :id LIMIT 1")
     suspend fun getRecipe(id: Int): RecipeEntity?
 
+    // Add Recipe
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(recipe: RecipeEntity)
 
+    // Update Recipe
     @Update
     suspend fun update(recipe: RecipeEntity)
 
+    // Delete Recipe
     @Delete
     suspend fun delete(recipe: RecipeEntity)
+
+    // Get Recipe Count
+    @Query("SELECT COUNT(*) FROM recipes")
+    suspend fun getRecipeCount(): Int
+
+    // Get Recipes Without Images
+    @Query("SELECT * FROM recipes WHERE image IS NULL")
+    suspend fun getRecipesWithoutImages(): List<RecipeEntity>
+
+    // Get Recipes By Title
+    @Query("SELECT * FROM recipes WHERE title LIKE '%' || :query || '%'")
+    fun searchRecipes(query: String): Flow<List<RecipeEntity>>
 }
