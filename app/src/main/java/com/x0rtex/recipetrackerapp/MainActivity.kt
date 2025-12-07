5e5442b9be8a01c7a0d5102585c8d87fc9144ca3
x0rtex <s00253368@atu.ie>
2025-12-07 15:01:26 +0000

    Updated MainActivity to use work manager and be able to change dark/light theme

--- app/src/main/java/com/x0rtex/recipetrackerapp/MainActivity.kt
+++ app/src/main/java/com/x0rtex/recipetrackerapp/MainActivity.kt
@@ -4,17 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import com.x0rtex.recipetrackerapp.data.models.UserSettings
+import com.x0rtex.recipetrackerapp.data.repository.SettingsManager
 import com.x0rtex.recipetrackerapp.ui.theme.RecipeTrackerAppTheme
+import com.x0rtex.recipetrackerapp.worker.WorkManagerSetup
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Initialise Work Manager
+        WorkManagerSetup.schedulePeriodicTasks(applicationContext)
+
         setContent {
-            RecipeTrackerAppTheme {
+            // Get settings to check dark mode
+            val context = LocalContext.current
+            val settingsManager = remember { SettingsManager(context) }
+            val settings by settingsManager.settingsFlow.collectAsState(initial = UserSettings())
+
+            // Set dark mode from settings
+            RecipeTrackerAppTheme(darkTheme = settings.isDarkTheme) {
                 RecipeApp()
             }
         }
